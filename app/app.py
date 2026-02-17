acad19a3fb844de2b68d1c125998f5aecef25e10
Rika Rostika Afipah <148519269+RikaProjects@users.noreply.github.com>
2026-02-18 01:23:32 +0700

    Update model loading to use os.path for file access
    Refactor model loading to use dynamic path.

--- app/app.py
+++ app/app.py
@@ -4,6 +4,7 @@ import pandas as pd
 import numpy as np
 import time
 import matplotlib.pyplot as plt
+import os
 
 # PAGE CONFIG
 
@@ -14,10 +15,16 @@ st.set_page_config(
 
 # LOAD MODEL
 
+import os
+
 @st.cache_resource
 def load_model():
-    with open("model_sales.pkl", "rb") as file:
+    base_path = os.path.dirname(__file__)
+    model_path = os.path.join(base_path, "model_sales.pkl")
+
+    with open(model_path, "rb") as file:
         model = pickle.load(file)
+
     return model
 
 model = load_model()
